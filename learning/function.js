152355cceb8ee112b270c45f25edad53c3dc8fe6
matics33 <matiascarba40@gmial.com>
2024-04-28 10:31:32 -0300

    esto es solo una subida de prueba

--- learning/function.js
+++ learning/function.js
@@ -30,15 +30,16 @@ console.log(suma());
 
 siguiente operacion pasando numero 1 y numero 2 como un paramentro*/
 
-var funcion = function () { };
+//var funcion = function () { };//
 
 function suma(numero1, numero2) {
-    console.log(numero1 + numero2);
+    (numero1 + numero2);
 };
+
 
 console.log(suma(240, 820));
 console.log(suma(112, 898));
 console.log(suma(740, 260));
 console.log(suma(425, 575));
 
-// mejor manera de hacer esta operacion //
+// mejor manera de hacer esta operacion 
