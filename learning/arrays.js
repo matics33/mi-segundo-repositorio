61032e6978f04a3ddc435070b0ea36505f0f11be
matics33 <matiascarba40@gmial.com>
2024-04-29 12:04:55 -0300

    cambios de carpetas

---
+++ learning/arrays.js
@@ -0,0 +1,28 @@
+const cities = [
+    "New York",
+    "Huston",
+    "Chicago"
+]
+
+console.log(cities)
+cities.push("Los Angeles");
+console.log(cities)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ learning/objects.js
@@ -0,0 +1,18 @@
+const myCat = {
+    name: 'mila',
+    color: 'black',
+    numberOfEyes: 1,
+    hasHair: false,
+    genre: 'Helicopeter',
+    humgry: 10.
+    eat: fuction() {
+        return console.log('Mi gato esta comiendo')
+    }
+}
+
+
+
+
+console.log("imprimimos el nombre de mi gato:" myCat.name)
+
+console.log("imprimimos el nombre de mi gato:" myCat.color)
